90f7f50405d623537c922203f6a83ceb9878b5a6
Aline Marques <alinecristina098@hotmail.com>
2021-02-25 15:20:21 -0300

    Adicionado o campo combo

--- INTERFACE GUI.py
+++ INTERFACE GUI.py
@@ -1,25 +1,24 @@
 from PySimpleGUI import PySimpleGUI as sg
 
-
 # Layout
 sg.theme('Reddit')
 layout = [
     [sg.Text('Nome do Livro '), sg.Input(key='Nlivro', size=(20, 1))],
-    [sg.Text('Sequencias    '), sg.Input(key='Qseque', size=(20, 1))],
+    [sg.Text('Quantidade de sequencia'), sg.Combo(values=list(range(21)), key='total_chars', default_value=0, size=(3, 1))],
     [sg.Text('Autor             '), sg.Input(key='autor', size=(20, 1))],
-    [sg.Text('Ano Lido        '), sg.Input(key='ano', size=(20, 1))],
-    [sg.Button('Registrar'), sg.Button('Sair')]
+    [sg.Text('Ano Lido        '), sg.Combo(values=list(range(2026)), key='ano', default_value=2020, size=(5, 1))],
+    [sg.Button('Registrar', key='botao', size=(10, 0)), sg.Button('Sair', key='sair', size=(10, 0))]
 ]
-
+val = 'total_chars'
+quan = int(10)
 
 # Janela
 janela = sg.Window('Tela de registro de livros', layout)
 # ler os eventos
 while True:
     eventos, valores = janela.read()
-    if eventos == sg.WIN_CLOSED or eventos == 'Sair':
-        break
-    if eventos == 'Registrar':
-        print('Livro registrar')
+    if eventos == sg.WIN_CLOSED or eventos == 'sair':
+        quit()
+    if eventos == 'botao':
+        print('Livro registrado')
         print([valores])
-        break
