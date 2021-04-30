5dcadf1c56bee0799ce6568b8bc7fefaf413287e
Aline Marques <67984559+line-mar8109@users.noreply.github.com>
2021-04-30 09:51:14 -0300

    Update prettytable_tabela.py

--- Aleatorios/prettytable_tabela.py
+++ Aleatorios/prettytable_tabela.py
@@ -3,8 +3,8 @@ from prettytable import PrettyTable
 
 my_table = PrettyTable(["Student Name", "Class", "Section", "Percentage"])
 
-my_table.add_row(["Leanord", "X", "B", "91.2%"])
-my_table.add_row(["Line", "X", "C", "94.2%"])
-my_table.add_row(["Leanord", "X", "B", "91.2%"])
-my_table.add_row(["Leanord", "X", "B", "91.2%"])
+my_table.add_row(["Leanord", "X", "A", "91.2%"])
+my_table.add_row(["Aline", "X", "B", "94.2%"])
+my_table.add_row(["Jily", "X", "C", "41.1%"])
+my_table.add_row(["Jeon", "X", "D", "34.0%"])
 print(my_table)
