366d55e82427bd14928056a83e66c8f4335ded53
Aline Marques <67984559+line-mar8109@users.noreply.github.com>
2021-05-04 11:01:21 -0300

    Update baixar_video_do_youtube.py

--- Aleatorios/baixar_video_do_youtube.py
+++ Aleatorios/baixar_video_do_youtube.py
@@ -1,18 +1,19 @@
 from pytube import YouTube
 
 # C:\Userd\aline\Downloads
-link = 'https://www.youtube.com/watch?v=_ysomCGaZLw'
+link = input("Link do video: ")
 path = input("Path aqui: ")
 yt = YouTube(link)
 
-
+print('*'*29)
 print('titulo: ', yt.title)
 print('Views: ', yt.views)
 print('Tempo de duração: ', yt.length, 'segundos')
 print('Avaliação: ', yt.rating)
+print('*'*29)
 
 ys = yt.streams.get_highest_resolution()
 
-print('Baixando...')
+print('Baixando........................')
 ys.download(path)
 print('Download completo!!!')
