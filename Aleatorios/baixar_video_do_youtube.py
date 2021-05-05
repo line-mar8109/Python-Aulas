db1aafb64c840e6c2b6db4c5a0f021d00bfa42e8
Aline Marques <alinecristina098@hotmail.com>
2021-05-05 19:03:33 -0300

    Adicionando animation

--- Aleatorios/baixar_video_do_youtube.py
+++ Aleatorios/baixar_video_do_youtube.py
@@ -1,7 +1,9 @@
 from pytube import YouTube
+import animation
+import time
 
  # C:\Users\aline\Videos\DownloaderdoYouTube
-link = input("Link do video: ")
+link = str(input("Link do video: "))
 yt = YouTube(link)
 
 print('*'*29)
@@ -11,7 +13,18 @@ print('Duration: ', yt.length, 'seconds')
 print('Assessment: ', yt.rating)
 print('*'*29)
 
+elipses = (
+    '....                 ', '...........              ', '...........          ',
+    '........             ', '..............           ', '...............      ',
+    '............         ', '...................      ', '..................   ',
+    '...................  ', '........................ ', '.....................'
+)
+@animation.wait(elipses, color='red')
+def long_running_function():
+    time.sleep(3)
+    return
+print('Baixando ', end='')
+long_running_function()
 
-print('Baixando.....................')
 yt.streams.get_highest_resolution().download('C://Users//aline//Videos//DownloaderdoYouTube')
 print('Download completo!!!')
